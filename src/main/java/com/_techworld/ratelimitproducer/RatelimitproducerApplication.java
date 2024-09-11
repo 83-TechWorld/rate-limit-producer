04d3fbe56dcc496b53aaf748770bc130f364efb1
83-TechWorld <sathishkumar.kanda@gmail.com>
2024-09-11 14:47:49 +0530

    initproject

---
+++ src/main/java/com/_techworld/ratelimitproducer/RatelimitproducerApplication.java
@@ -0,0 +1,13 @@
+package com._techworld.ratelimitproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimitproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatelimitproducerApplication.class, args);
+	}
+
+}
